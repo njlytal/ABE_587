645b37d69eb353328191789e4506bc38e21e8acf
njlytal <njlytal@ucdavis.edu>
2015-09-08 10:08:14 -0700

    Script 4 Added
    Includes the script for tabs and newlines

---
+++ homework2/perl_pr4.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+print "Come with me,\n\t and you'll be \n\t\t in a place of pure imagination.\n";
+print "Take one look,\n\t and you'll see\n\t\t into your imagination.\n";
