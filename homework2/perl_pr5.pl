da97e0689d528fdf0f38fdceccbb6cb8945b4fe7
njlytal <njlytal@ucdavis.edu>
2015-09-08 21:10:51 -0700

    problems 5-7
    Last few Perl problems added. Check incoming soon.

---
+++ homework2/perl_pr5.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# Problems 5 & 6
+
+print "What would you do for a Klondike bar?\n";
+
+print 'What would you do for a Klondike bar?\n';
---
+++ homework2/perl_pr6.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# Comparisons
+# Tabs & Newlines
+
+print "What would you do\n\t for a Klondike bar?\n";
+print 'What would you do\n\t for a Klondike bar?\n';
+print "\n";
+
+# Variables
+
+my $K = 'Klondike';
+my $u = 'you';
+
+print "What would $u do for a $K bar?\n";
+print 'What would $u do for a $K bar?\n';
+print "\n";
+# Math
+
+print "What would you do for 3*2 Klondike bars?\n";
+print 'What would you do for 3*2 Klondike bars?\n';
+print "\n";
+
+# Printing Math
+print "What would you do for ", 3 * 2, " Klondike bars?\n";
+print 'What would you do for ', 3 * 2, ' Klondike bars?\n';
+print "\n";
+
