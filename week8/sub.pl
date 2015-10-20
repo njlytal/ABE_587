67f1fc30e4b9b552053c7dd158212c29e5d24a13
Nick Lytal <njlytal@email.arizona.edu>
2015-10-20 00:23:08 -0700

    text files added
    Added in files used for the scripts

---
+++ week8/sub.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+
+### PSEUDOCODE ###
+1. Make sure
+
+
+unless(@ARGV)
+{
+    die "Please enter a name!"\n;
+}
+
+open my $fh, '<', 'nobody.txt';
+
