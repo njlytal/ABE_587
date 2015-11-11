bc528829aa4197105855fcf560fa292ac85c3438
Nick Lytal <njlytal@email.arizona.edu>
2015-11-11 15:52:29 -0700

    Script rough draft complete
    Output appears promising, need to test to be sure, however

--- week11/01-bio-searchio.pl
+++ week11/01-bio-searchio.pl
@@ -7,10 +7,17 @@ use feature 'say';
 use Getopt::Long;
 use Pod::Usage;
 use Bio::SeqIO;
-use Cwd 'cwd';
-use File::Spec::Functions 'catfile';
-use File::Basename;
-use File::Path 'make_path';
+use Bio::SearchIO;
+
+
+# WARNING: Help doesn't seem to work for some reason, may need to rebuild
+
+#PSUEDOCODE
+#Read in a query file
+#Use max_significance() to get all reads <= 1e-50
+#THEN print the following from those reads in tab-delim format:
+#query name, hit name, hsp evalue
+
 
 main();
 
@@ -24,9 +31,39 @@ sub main {
             -verbose => $args{'man_page'} ? 2 : 1
         });
     }; 
-    
-    say "OK";
 
+	unless(@ARGV){
+		die "Please select a blast file to search through.\n";
+	}
+    
+	# Read in filename--this is the only argument
+	my $file = shift @ARGV;
+
+	# Establish sig. level to isolate
+	my $max_sig = 1e-50;
+
+	# say "Now reading $file...";
+	# say "Searching for HSPs of hits with max sig. <= $max_sig...";
+
+	# SearchIO object created from input file
+	my $in = Bio::SearchIO->new(-file => $file,
+								 -format => 'blast');
+
+	# Header for the output
+	say "query \t hit \t evalue";
+	# Cycle through queries
+	while (my $result = $in->next_result){
+		# Cycle through hits
+		while(my $hit = $result->next_hit){
+			# Cycle through HSPs
+			while(my $hsp = $hit->next_hsp){
+				if($hsp->significance <= $max_sig){
+					say $hsp->query_string, "\t", $hsp->hit_string, "\t", $hsp->evalue;
+				}
+
+			}
+		}
+	}
 
 }
 
@@ -52,10 +89,10 @@ __END__
 
 =head1 NAME
 Usage:
-    (Example line here)
-=head1 SYNOPSIS
+	01-bio-searchio.pl blast.out
 
-  (Name of file)
+=head1 SYNOPSIS
+  01-bio-searchio.pl
 
 Options (defaults in parentheses):
   --help    Show brief help and exit
@@ -63,7 +100,12 @@ Options (defaults in parentheses):
 
 =head1 DESCRIPTION
 
-Describe code functions here
+Parses through a Blast output file. For hits with significance less than
+or equal to 1e-50, it retrieves every High-scoring Segment Pair (HSP)
+and lists the following in tab-delimited form:
+* Query Name
+* Hit Name
+* HSP Evalue
 
 =head1 SEE ALSO
 
